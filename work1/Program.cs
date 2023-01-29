5294421ff9d9d560177887b878d933c20dc31aef
unknown <vitalii.programm@gmail.com>
2023-01-29 13:00:08 +0300

    задача 2

--- work1/Program.cs
+++ work1/Program.cs
@@ -3,6 +3,20 @@
 // a = 5; b = 7 -> max = 7
 // a = 2 b = 10 -> max = 10
 // a = -9 b = -3 -> max = -3
+System.Console.Write("Введите первое число: ");
+int numA = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите второе число: ");
+int numB = Convert.ToInt32(Console.ReadLine());
+
+if (numA > numB){
+    System.Console.WriteLine($"Число {numA} больше числа {numB}");
+}
+else if (numB > numA){
+    System.Console.WriteLine($"Число {numB} больше числа {numA}");
+}
+else{
+    System.Console.WriteLine($"Число {numA} равно числу {numB}");
+}
 
 // Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
 
