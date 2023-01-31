7ae1633f1fafef3353bed25e67ac28882719b51e
unknown <vitalii.programm@gmail.com>
2023-01-31 16:03:20 +0300

    задача 4

--- work1/Program.cs
+++ work1/Program.cs
@@ -3,26 +3,41 @@
 // a = 5; b = 7 -> max = 7
 // a = 2 b = 10 -> max = 10
 // a = -9 b = -3 -> max = -3
-System.Console.Write("Введите первое число: ");
-int numA = Convert.ToInt32(Console.ReadLine());
-System.Console.Write("Введите второе число: ");
-int numB = Convert.ToInt32(Console.ReadLine());
+// -------------------------------
+// System.Console.Write("Введите первое число: ");
+// int numA = Convert.ToInt32(Console.ReadLine());
+// System.Console.Write("Введите второе число: ");
+// int numB = Convert.ToInt32(Console.ReadLine());
 
-if (numA > numB){
-    System.Console.WriteLine($"Число {numA} больше числа {numB}");
-}
-else if (numB > numA){
-    System.Console.WriteLine($"Число {numB} больше числа {numA}");
-}
-else{
-    System.Console.WriteLine($"Число {numA} равно числу {numB}");
-}
+// if (numA > numB){
+//     System.Console.WriteLine($"Число {numA} больше числа {numB}");
+// }
+// else if (numB > numA){
+//     System.Console.WriteLine($"Число {numB} больше числа {numA}");
+// }
+// else{
+//     System.Console.WriteLine($"Число {numA} равно числу {numB}");
+// }
 
 // Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
 
 // 2, 3, 7 -> 7
 // 44 5 78 -> 78
 // 22 3 9 -> 22
+System.Console.Write("Введите первое число: ");
+int numA = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите второе число: ");
+int numB = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите второе число: ");
+int numC = Convert.ToInt32(Console.ReadLine());
+
+// if (numA > numB && numA > numC) {
+//     System.Console.WriteLine($"Максимальное число {numA}");
+// }
+int [] i = {numA, numB, numC};
+int maxValue = i.Max();
+
+System.Console.WriteLine(maxValue);
 
 // Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
 
